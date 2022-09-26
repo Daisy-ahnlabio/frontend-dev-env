bd2f6f3b95072adebe4b638241d7f97f112b9d67
daisy-ahnlabio <daisy@ahnlab.io>
2022-09-26 14:23:25 +0900

    09.26 웹팩(Webpack) - 심화편

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require("path");
 const webpack = require("webpack");
-
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const apiMocker = require("connect-api-mocker");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+const mode = process.env.NODE_ENV || "development";
 
 module.exports = {
-  mode: "development",
+  mode,
   entry: {
     main: "./src/app.js"
   },
@@ -16,12 +19,23 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve("./dist")
   },
+  devServer: {
+    overlay: true,
+    stats: "errors-only",
+    // before: (app, server, compiler) => {
+    //   app.use(apiMocker('/api', 'mocks/api'));
+    // },
+    proxy: {
+      "/api": "http://localhost:8081"
+    },
+    hot: true
+  },
   module: {
     rules: [
       {
         test: /\.(scss|css)$/,
         use: [
-          process.env.NODE_ENV === "production"
+          mode === "production"
             ? MiniCssExtractPlugin.loader // 프로덕션 환경
             : "style-loader", // 개발 환경
           "css-loader",
@@ -38,8 +52,8 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        use: ["babel-loader"],
         exclude: /node_modules/,
+        loader: "babel-loader" // 바벨 로더를 추가한다
       }
     ]
   },
@@ -47,23 +61,48 @@ module.exports = {
     new webpack.BannerPlugin({
       banner: `빌드 날짜: ${new Date().toLocaleString()}`
     }),
+    new webpack.DefinePlugin({}),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+        env: mode === "development" ? "(개발용)" : ""
       },
       minify:
-        process.env.NODE_ENV === "production"
+        mode === "production"
           ? {
-            collapseWhitespace: true, // 빈칸 제거
-            removeComments: true // 주석 제거
-          }
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true // 주석 제거
+            }
           : false,
-      hash: process.env.NODE_ENV === "production"
+      hash: mode === "production"
     }),
     new CleanWebpackPlugin(),
-    ...(process.env.NODE_ENV === "production"
+    new CopyPlugin([
+      {
+        from: "./node_modules/axios/dist/axios.min.js",
+        to: "./axios.min.js" // 목적지 파일에 들어간다
+      }
+    ]),
+    ...(mode === "production"
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
       : [])
-  ]
+  ],
+  optimization: {
+    minimizer:
+      mode === "production"
+        ? [
+            new OptimizeCSSAssetsPlugin(),
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true // 콘솔 로그를 제거한다
+                }
+              }
+            })
+          ]
+        : []
+  },
+  externals: {
+    axios: "axios"
+  }
 };
